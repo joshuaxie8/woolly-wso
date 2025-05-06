a7775973f11a4ddff64f6e550aefddd60d67f465
Joshua Xie <jx4@williams.edu>
2025-05-06 14:14:03 -0400

    initial file skeletons; subject to changes

---
+++ bktree/MetricFunctions.java
@@ -0,0 +1,15 @@
+package bktree;
+
+import java.util.*;
+
+public class MetricFunctions {
+	// Levenshtein distance
+	public static int Lev(String a, String b) {
+		return 0;
+	}
+
+	// Damerau-Levenshtein distance
+	public static int DamLev(String a, String b) {
+		return 0;
+	}
+}

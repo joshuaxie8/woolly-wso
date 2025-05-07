e26620c263424fa8cb22497823e9b8ee82b14a53
Joshua <jx4@williams.edu>
2025-05-07 15:46:39 -0400

    some method modifications prioritizing flexibility

--- bktree/BKTree.java
+++ bktree/BKTree.java
@@ -8,24 +8,25 @@ public class BKTree<T, Data> implements Tree<T> {
 		Data data;
 		Map<Integer, Node> children = new HashMap<>();
 
-		// to do: define data object (class year, major, other info etc.)
-		// or should we store student data in a separate map?
-		public Node(T value, Data data) {
+		// TO-DO: implement storage of individual data in hash map outside of bk-tree
+		// (good for actual implementation to WSO database)
+		// data field may not be necessary
+		public Node(T value) {
 			this.value = value;
-			this.data = data;
 		}
 	}
 
 	private Node root;
 	private int size = 0;
-	private Comparator<T> c;
+	private Comparator<T> c; // a BK-tree comparator must return a range of integer values, not just -1, 0, and 1
 
 	public BKTree(Comparator<T> comparator) {
 		c = comparator;
 	}
 
 	public boolean isEmpty() {
-		return true;
+		if (size == 0) return true;
+		return false;
 	}
 
 	public int size() {
@@ -39,31 +40,34 @@ public class BKTree<T, Data> implements Tree<T> {
 	}
 
 	// gets the node corresponding to the given value; returns null if no match is found
+	// return type is TBD - currently Node for generalizability
 	public Node get(T value) {
 		return null;
 	}
 
-	// decide which one is better
-	public void insert(T value) { // insertion with value
+	// adds a new node to the BK-tree
+	public void insert(T value) {
+		Node node = new Node(value);
+	}
+	public void insert(Node node) {
 
 	}
 
-	public void insert(Node node) { // insertion with node
-
-	}
-
+	// removes a node from the BK-tree - returns true if deletion is successful
 	public boolean delete(T value) {
 		return contains(value);
 	}
 
-	// edits a node - in practice just removes the old node and inserts one with the new value
-	public boolean edit(Node node, T newValue) {
+	// updates a node - in practice just removes the old node and inserts one with the new value
+	// returns true if edit is successful
+	public boolean update(Node node, T newValue) {
 		return false;
 	}
 
 	// searches for all stored values within a distance d from the given value (fuzzy search)
-	public ArrayList<T> search(T value, int d) {
-		return new ArrayList<T>();
+	// return type is TBD - currently Node for generalizability
+	public ArrayList<Node> search(T value, int d) {
+		return new ArrayList<Node>();
 	}
 
 	public static void main(String[] args) {

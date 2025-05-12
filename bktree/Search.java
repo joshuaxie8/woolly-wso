ee25f25eab68174e9bc1bf717cd3b2f0292e26d3
Joshua <jx4@williams.edu>
2025-05-11 22:59:30 -0400

    basic user input for in-class demo

---
+++ bktree/Search.java
@@ -0,0 +1,75 @@
+package bktree;
+
+import java.util.*;
+
+// this is our main file
+public class Search {
+	BKTree<String, String> bk; 	// bk tree!
+	Trie<String, String> t;		// trie!
+	People p = new People();	// automatically ingests people
+
+	public Search(DistanceMetric<String> c) {
+		bk = new BKTree<String, String>(c);
+		t = new Trie<String, String>();
+	}
+
+	public ArrayList<String> getResults(String query, int maxDist, int priority) {
+		return null;
+	}
+
+	public ArrayList<String> simplify(String s) {
+		StringBuilder sb = new StringBuilder(s); // mutable StringBuilder to save memory
+		return null;
+	}
+
+	public static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static int getUserInt() {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()) { scanner.nextLine(); }
+        return scanner.nextInt();
+    }
+
+	public static void main(String[] args) {
+		Search tests = new Search(MetricFunctions.Lev);
+
+		for (Person person : tests.p.peopleList) { // iterates through each person
+			//System.out.println(person.getFirstName());
+			tests.t.insert(person.getFirstName().toLowerCase()); // trie insertion
+			tests.bk.insert(person.getFirstName().toLowerCase()); // bk-tree insertion
+		}
+
+		while (true) {
+			System.out.print("Enter a name: ");
+			String a = getUserInput(); a = a.toLowerCase();
+			System.out.print("Show results within __ characters: ");
+			int d = getUserInt();
+
+			ArrayList<String> exactMatches = tests.t.traverseVals(tests.t.probe(a), a);
+			System.out.println("Exact matches: ");
+			for (String s : exactMatches) {
+				System.out.println(s);
+			}
+
+			ArrayList<String> fuzzyMatches = tests.bk.search(a, d);
+			System.out.println("Fuzzy matches: ");
+			for (String s : fuzzyMatches) {
+				System.out.println(s);
+			}
+
+
+		}
+
+		// ArrayList<String> print = tests.t.traverseData();
+        // for (int i = 0; i < print.size(); i++) {
+        //     System.out.println(print.get(i));
+        // }
+        // print = tests.bk.traverseVals();
+        // for (int i = 0; i < print.size(); i++) {
+        //     System.out.println(print.get(i));
+        // }
+	}
+}

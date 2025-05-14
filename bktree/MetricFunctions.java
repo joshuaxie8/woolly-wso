c4a379b6bb6a87c3cb3fcf90f5510165448de829
Joshua Xie <jx4@williams.edu>
2025-05-14 19:39:10 -0400

    osa method implementation

--- bktree/MetricFunctions.java
+++ bktree/MetricFunctions.java
@@ -6,8 +6,7 @@ public class MetricFunctions {
 
 	// Levenshtein distance
 	// Runs in O(m*n) where m and n are the lengths of a and b
-	public static DistanceMetric<String> Lev = new DistanceMetric<String>() {
-		@Override
+	public static DistanceMetric<String> lev = new DistanceMetric<String>() {
 		public int compute(String a, String b) {
 			a = a.toLowerCase();
 			b = b.toLowerCase();
@@ -39,21 +38,50 @@ public class MetricFunctions {
 		}
 	};
 
-	// Damerau-Levenshtein distance (unfinished)
+	// Damerau-Levenshtein distance methods
 
-	public static int DamLev(String a, String b) {
-		int m = a.length();
-		int n = b.length();
+	// Computes the Optimal String Alignment (OSA) distance (each substring can only be edited once)
+	// Only adjacent transpositions are allowed
+	public static DistanceMetric<String> osa = new DistanceMetric<String>() {
+		public int compute(String a, String b) {
+			int m = a.length();
+			int n = b.length();
 
-		if (m == 0) return n;
-		if (n == 0) return m;
+			if (m == 0) return n;
+			if (n == 0) return m;
 
-		return 0;
-	}
+			int[] d0 = new int[n + 1];
+			int[] d1 = new int[n + 1];
+			int[] d2 = new int[n + 1];
+
+			Arrays.setAll(d1, i -> i); // d1 = {0, 1, 2, ... , n};
+
+			for (int i = 0; i < m; i++) {
+				d2[0] = i;
+
+				for (int j = 0; j < n; j++) {
+					int del = d1[j + 1] + 1;
+					int ins = d2[j] + 1;
+					int sub = (a.charAt(i) == b.charAt(j)) ? d1[j] : d1[j] + 1;
+
+					d2[j + 1] = Math.min(del, Math.min(ins, sub));
+
+					if (i > 0 && j > 0 && a.charAt(i) == b.charAt(j - 1) && a.charAt(i - 1) == b.charAt(j)) {
+						d2[j + 1] = Math.min(d2[j + 1], d0[j - 1] + 1);
+					}
+				}
+				int[] temp = d0;
+				d0 = d1;
+				d1 = d2;
+				d2 = temp;
+			}
+			return d1[n];
+		}
+	};
 
 	// testing
 	public static void main(String[] args) {
-		System.out.println(MetricFunctions.Lev.compute("aardvark", "AAAAAAAK"));
+		System.out.println(MetricFunctions.Lev.compute("aaron", "araon"));
 
 	}
 }

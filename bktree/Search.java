6ff594226c8fd3705f902162681c11fc4634151a
Joshua <jx4@williams.edu>
2025-05-15 00:05:12 -0400

    cleaned up initialization

--- bktree/Search.java
+++ bktree/Search.java
@@ -6,33 +6,42 @@ import java.text.Normalizer;
 // this is our main file
 public class Search {
 	People p = new People();	// automatically ingests people
-	BKTree<String, String> bk; 	// bk tree!
-	Trie<String, String> t;		// trie!
+	BKTree<String, Integer> bk; 	// bk tree!
+	Trie<String, Integer> t;		// trie!
+	HashMap<Integer, Person> idToPerson = new HashMap<>(8192);
+	HashMap<Person, Integer> personToId = new HashMap<>(8192);
+	int id = 0;
 
 	public Search(DistanceMetric<String> c) {
-		bk = new BKTree<String, String>(c);
-		t = new Trie<String, String>();
+		bk = new BKTree<String, Integer>(c);
+		t = new Trie<String, Integer>();
+
+		// initialization
+		for (Person person : p.peopleList) {
+			idToPerson.put(id, person);
+			personToId.put(person, id);
+
+			ArrayList<String> ar = simplify(person.getFirstName());
+			for (String s : ar) {
+				t.insert(s, id);
+				bk.insert(s, id);
+			}
+			id++;
+		}
 	}
 
-	public ArrayList<String> getResults(String query, int maxDist, int priority) {
-		return null;
-	}
-
-	/*
-	STRING SIMPLIFICATION - FOR BOTH USER INPUT AND STORED VALUES
-
-	given a String s, returns an array list of all normalized, "reasonable" parsings of s
-
-	ex.
-	simplify("Waage-Pickle") -> {"waagepickle", "waage pickle"}
-	simplify("José") -> {"jose"}
-	simplify("O'Connor") -> {"oconnor", "o connor"}
-	simplify("J. B.") -> {"jb", "j b"}
-	simplify("John A. Doe") -> {"john a doe"} - only concatenate for single letter pairs?
-
-	(tentative)
-	For stored names, insert all results into BK-tree/Trie, but all with the same value field for later retrieval
-	*/
+	/**
+	 * STRING SIMPLIFICATION - FOR BOTH USER INPUT AND STORED VALUES
+	 * Given a String s, returns an array list of normalized, "reasonable" parsings of s
+	 * ex.
+	 * simplify("Waage-Pickle") -> {"waagepickle", "waage pickle"}
+	 * simplify("José") -> {"jose"}
+	 * simplify("O'Connor") -> {"oconnor", "o connor"}
+	 * simplify("J. B.") -> {"jb", "j b"}
+	 * simplify("John A. Doe") -> {"john a doe"} - only concatenate for single letter pairs?
+	 * 
+	 * For stored names, insert all results into BK-tree/Trie, but all with the same value field for later retrieval?
+	 */
 	public static ArrayList<String> simplify(String s) {
 		String norm = Normalizer.normalize(s, Normalizer.Form.NFD)
 			.replaceAll("\\p{InCombiningDiacriticalMarks}+", ""); // removes diacritics, e.g. José -> Jose
@@ -73,51 +82,34 @@ public class Search {
 
 		System.out.println(Search.simplify("José"));
 		System.out.println(Search.simplify("J. B."));
-		// for (Person person : tests.p.peopleList) { // iterates through each person
-		// 	//System.out.println(person.getFirstName());
-		// 	tests.t.insert(person.getFirstName().toLowerCase(), person.getFullName()); // trie insertion
-		// 	tests.bk.insert(person.getFirstName().toLowerCase(), person.getFullName()); // bk-tree insertion
-		// }
 
-		// while (true) {
-		// 	System.out.print("Enter a name: ");
-		// 	String a = getUserInput(); a = a.toLowerCase();
-		// 	System.out.print("Show results within __ characters: ");
-		// 	int d = getUserInt();
+		while (true) {
+			System.out.print("Enter a name: ");
+			String a = getUserInput(); a = a.toLowerCase();
+			System.out.print("Show results within __ characters: ");
+			int d = getUserInt();
 
-		// 	ArrayList<String> exactMatches = tests.t.traverseVals(tests.t.probe(a), a);
-		// 	System.out.println("Exact prefix matches: ");
-		// 	if (exactMatches.size() > 4) {
-		// 		for (int i = 0; i < 3; i++) {
-		// 			System.out.println(exactMatches.get(i));
-		// 		}
-		// 		System.out.print(exactMatches.size() - 3); System.out.println(" more results");
-		// 	}
-		// 	else {
-		// 		for (String s : exactMatches) {
-		// 			System.out.println(s);
-		// 		}
-		// 	}
+			ArrayList<Integer> exactMatches = tests.t.traverseVals(tests.t.probe(a), a);
+			System.out.println("Exact prefix matches: ");
+			if (exactMatches.size() > 4) {
+				for (int i = 0; i < 3; i++) {
+					System.out.println(tests.idToPerson.get(i).getFullName());
+				}
+				System.out.print(exactMatches.size() - 3); System.out.println(" more results");
+			}
+			else {
+				for (int i : exactMatches) {
+					System.out.println(tests.idToPerson.get(i).getFullName());
+				}
+			}
 
-		// 	ArrayList<String> fuzzyMatches = tests.bk.fuzzyVals(a, d, false);
+			ArrayList<Integer> fuzzyMatches = tests.bk.fuzzyVals(a, d, false);
 
-
-		// 	// ArrayList<String> fuzzyMatches = tests.bk.searchData(a, d);
-		// 	System.out.println("Fuzzy matches: ");
-		// 	for (String s : fuzzyMatches) {
-		// 		System.out.println(s);
-		// 	}
-
-		// 	// TO DO: 
-		// }
-
-		// ArrayList<String> print = tests.t.traverseData();
-        // for (int i = 0; i < print.size(); i++) {
-        //     System.out.println(print.get(i));
-        // }
-        // print = tests.bk.traverseVals();
-        // for (int i = 0; i < print.size(); i++) {
-        //     System.out.println(print.get(i));
-        // }
+			// ArrayList<String> fuzzyMatches = tests.bk.searchData(a, d);
+			System.out.println("Fuzzy matches: ");
+			for (int i : fuzzyMatches) {
+				System.out.println(tests.idToPerson.get(i).getFullName());
+			}
+		}
 	}
 }

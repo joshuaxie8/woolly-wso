60ab4b78377daa0614f4a906057da3eda6a736e8
Joshua Xie <jx4@williams.edu>
2025-05-19 19:04:45 -0400

    added failsafe for getValues()

--- woolly-wso/Trie.java
+++ woolly-wso/Trie.java
@@ -34,7 +34,7 @@ public class Trie<K, V> implements Tree<K, V> {
     }
 
     public ArrayList<V> getValues(Node node) {
-        if (node.values == null) return new ArrayList<V>();
+        if (node == null || node.values == null) return new ArrayList<V>();
         return node.values;
     }
 
